dcbf1f6f18c926cb6f58880b20584f1c3587fc70
SultanIndian007 <amantiwari201608@gmail.com>
2023-03-08 01:08:41 +0530

    1:33:10_done

---
+++ Static_page/Body.js
@@ -0,0 +1,16 @@
+import React from "react"
+function Body(){
+    return(
+        <div>
+        <h1>Reasons I am excited to learn React</h1>
+        <ol>
+            <li>I intend to surpa1s my friend and eternal rival Ganesh in React then beat Jwala</li>
+            <li>React is fun to integrate and is used widely in MERN stack so ill do it too</li>
+            <li>My name is Itachi</li>
+            <li>I have to master REACT in order to survive</li>
+        </ol>
+        </div>
+    )
+}
+
+export default Body

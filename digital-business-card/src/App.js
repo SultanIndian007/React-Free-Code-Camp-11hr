66853ef7757911bd45f15ec7e66b52825a661187
SultanIndian007 <amantiwari201608@gmail.com>
2023-03-11 19:25:21 +0530

    Added-business-card-majorly-done

---
+++ digital-business-card/src/App.js
@@ -0,0 +1,14 @@
+import Intro from "../src/Components/Intro"
+import Body from "../src/Components/Body"
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      < Intro />
+      <Body />
+    </div>
+  )
+}
+
+export default App;

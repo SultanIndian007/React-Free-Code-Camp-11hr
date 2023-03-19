035c987682892f28fce88b4976e0ea4b4700b76c
SultanIndian007 <amantiwari201608@gmail.com>
2023-03-19 20:12:01 +0530

    changes2

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -6,24 +6,24 @@ import Card from '../src/Components/Card'
 import data from "./data"
 
 export default function App() {
-  // <Hero />
-const cards = data.map(item => {
-return (
-  <Card 
+  const cards = data.map(item => {
+    return (
+      <Card 
       img={item.coverImg}
       rating={item.stats.rating}
       reviewCount={item.stats.reviewCount}
       location={item.location}
       title={item.title}
       price={item.price}
-  />
-)
-})        
-
-return (
-<div>
-  <Navbar />
-  {cards}
-</div>
+      />
+      )
+    })        
+    
+    return (
+      <div>
+        <Navbar />
+        <Hero />
+        {cards}
+      </div>
 )
 }
